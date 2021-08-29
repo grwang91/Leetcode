884b0ae9c0893f610180f05286315ea769e48578
grwang <53140990+grwang91@users.noreply.github.com>
2021-08-29 21:01:24 +0900

    Time: 5 ms (73.30%), Space: 48 MB (91.01%) - LeetHub

--- maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,17 +1,13 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
-        
-        long evenSum = 0;
-        long oddSum = nums[0];
-        
-        for (int i=1; i<nums.length; i++) {
-            long tmpEven = oddSum - nums[i];
-            long tmpOdd = evenSum + nums[i];
-            
-            evenSum = Math.max(evenSum, tmpEven);
-            oddSum = Math.max(oddSum, tmpOdd);
+
+        long odd= nums[0], even=0;
+        for(int i=1; i<nums.length; i++)
+        {
+            long o= Math.max(odd,even+ nums[i]);
+            long e= Math.max(even,odd- nums[i]);
+            odd=o; even=e;
         }
-        
-        return oddSum;
+        return Math.max(odd,even);
     }
 }
