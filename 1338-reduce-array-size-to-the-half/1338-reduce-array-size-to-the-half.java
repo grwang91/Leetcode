e106aa73f5d1a49e3c057772ba1a0de2ed3e8262
grwang <53140990+grwang91@users.noreply.github.com>
2022-01-04 22:48:53 +0900

    Time: 157 ms (5.34%), Space: 113.3 MB (5.34%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,27 +1,36 @@
 class Solution {
     public int minSetSize(int[] arr) {
         Map<Integer, Integer> hash = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         for (int num : arr) {
             hash.put(num, hash.getOrDefault(num,0)+1);
         }
-        
-        for(int key : hash.keySet()) {
-            pq.add(hash.get(key));
-        }
-        int cnt = 0;
         int len = 0;
-        while(!pq.isEmpty()) {
-            len+=pq.poll();
-            cnt++;
+        for(int key : hash.keySet()) {
+            len+=hash.get(key);
+            pq.add(hash.get(key));
             
-            if(len >= arr.length/2) {
-                return cnt;
+            while(!pq.isEmpty()) {
+                if(len-pq.peek() >= arr.length/2) {
+                    len-=pq.poll();
+                } else {
+                    break;
+                }
             }
         }
+//         int cnt = 0;
         
-        return 0;
+//         while(!pq.isEmpty()) {
+//             len+=pq.poll();
+//             cnt++;
+            
+//             if(len >= arr.length/2) {
+//                 return cnt;
+//             }
+//         }
+        
+        return pq.size();
         
     }
 }
