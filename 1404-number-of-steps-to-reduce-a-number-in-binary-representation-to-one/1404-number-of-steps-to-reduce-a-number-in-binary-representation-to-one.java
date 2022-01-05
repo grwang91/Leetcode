15bad2228669b52f8818acc426a51b41db8b980e
grwang <53140990+grwang91@users.noreply.github.com>
2022-01-05 23:16:48 +0900

    Time: 0 ms (100.00%), Space: 37.1 MB (48.80%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,34 +1,26 @@
 class Solution {
     public int numSteps(String s) {
-        int ans = 0;
-        while(!s.equals("1")) {
-            ans++;
-            if(s.charAt(s.length()-1)=='1') {
-                s = add(s);
-            } else {
-                s = s.substring(0,s.length()-1);
+        int c=0,ans=0;
+        if(s.length()==1)
+            return 0;
+        
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(i==0 && s.charAt(i)=='1' && c==0)
+                break;
+                
+            int tmp= Character.getNumericValue(s.charAt(i))  +c;
+            if(tmp%2==0)
+            {
+                c=tmp/2;
+                ans++;
+            }
+            else
+            {
+                ans+=2;
+                c=1;
             }
         }
-        
         return ans;
-    }
-    
-    private String add(String s) {
-        char[] arr = s.toCharArray();
-        
-        boolean adder = true;
-        for (int i=arr.length-1; i>=0; i--) {
-            if(adder) {
-                if(arr[i] == '0') {
-                    arr[i] = '1';
-                    adder = false;
-                } else {
-                    arr[i] = '0';
-                    adder = true;
-                }
-            }
-        }
-        
-        return adder? "1"+new String(arr) : new String(arr); 
     }
 }
