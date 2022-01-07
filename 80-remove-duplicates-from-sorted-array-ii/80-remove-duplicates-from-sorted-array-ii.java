53c0ef15844ca7bf55aa2de71b1a95f69fb941f3
grwang <53140990+grwang91@users.noreply.github.com>
2022-01-07 13:06:57 +0900

    Time: 0 ms (100.00%), Space: 38.7 MB (98.62%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -1,24 +1,26 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int ans = nums.length;
-        
-        int currentNum = -10001;
-        int cnt = 0;
-        for (int i=0; i<nums.length; i++) {
-            if(nums[i] != currentNum) {
-                cnt = 1;
-                currentNum = nums[i];
-                continue;
-            }
-            
-            if(++cnt > 2) {
-                nums[i] = Integer.MAX_VALUE;
-                ans--;
-            }
+
+   int pointer = 0;
+    int count = 0;
+    int temp = nums[0];
+    
+    for(int i = 0; i < nums.length; i++){
+
+        if(nums[i] == temp){
+            count++;
+
+        }else{
+            temp = nums[i];
+            count = 1;
         }
         
-        Arrays.sort(nums);
-        
-        return ans;
+        if(count <= 2){
+            nums[pointer] = nums[i];
+            pointer++;
+        }
     }
+    
+    return pointer;
+}
 }
