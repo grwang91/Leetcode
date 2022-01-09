fb57ed873cd6becbe4086995a99a453706f75b13
grwang <53140990+grwang91@users.noreply.github.com>
2022-01-09 18:05:49 +0900

    Time: 15 ms (11.90%), Space: 45.3 MB (20.51%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,51 +1,41 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Map<TreeNode, Integer> pMap = new HashMap<>();
-        Map<TreeNode, Integer> qMap = new HashMap<>();
-        int maxDepth = -1;
-        TreeNode ans = null;
-        
-        findNode(root, p.val, 0, pMap);
-        findNode(root, q.val, 0, qMap);
-        
-        for (TreeNode key : pMap.keySet()) {
-            if(qMap.containsKey(key)) {
-                if(maxDepth < pMap.get(key)) {
-                    maxDepth = pMap.get(key);
-                    ans = key;
-                }
-            }
-        }
-        
-        return ans;
+
+    private TreeNode ans;
+
+    public Solution() {
+        // Variable to store LCA node.
+        this.ans = null;
     }
-    
-    private boolean findNode(TreeNode root, int target, int depth, Map<TreeNode, Integer> map) {
-        if(root == null) {
+
+    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
+
+        // If reached the end of a branch, return false.
+        if (currentNode == null) {
             return false;
         }
-        
-        map.put(root, depth);
-        if(root.val == target){
-            return true;
+
+        // Left Recursion. If left recursion returns true, set left = 1 else 0
+        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+
+        // Right Recursion
+        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+
+        // If the current node is one of p or q
+        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+
+
+        // If any two of the flags left, right or mid become True
+        if (mid + left + right >= 2) {
+            this.ans = currentNode;
         }
-        
-        boolean foundLeft = findNode(root.left, target, depth+1, map);
-        if(foundLeft) return true;
-        boolean foundRight = findNode(root.right, target, depth+1, map);
-        if(!foundRight) {
-           map.remove(root); 
-        }
-        return foundRight;
-        
+
+        // Return true if any one of the three bool values is True.
+        return (mid + left + right > 0);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Traverse the tree
+        this.recurseTree(root, p, q);
+        return this.ans;
     }
 }
