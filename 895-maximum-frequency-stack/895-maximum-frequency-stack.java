16e02617c13b8911754987119872eace90e35801
grwang <53140990+grwang91@users.noreply.github.com>
2022-01-16 18:32:43 +0900

    Time: 73 ms (13.92%), Space: 72.9 MB (31.19%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.java
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -1,28 +1,37 @@
 class FreqStack {
     Map<Integer, Integer> freq;
-    Map<Integer, Stack<Integer>> group;
-    int maxfreq;
+    Map<Integer, Stack<Integer>> stacks;
+    int maxFreq;
 
     public FreqStack() {
-        freq = new HashMap();
-        group = new HashMap();
-        maxfreq = 0;
+        freq = new HashMap<>();
+        stacks = new HashMap<>();
+        maxFreq = 0;
     }
-
-    public void push(int x) {
-        int f = freq.getOrDefault(x, 0) + 1;
-        freq.put(x, f);
-        if (f > maxfreq)
-            maxfreq = f;
-
-        group.computeIfAbsent(f, z-> new Stack()).push(x);
+    
+    public void push(int val) {
+        int f = freq.getOrDefault(val,0)+1;
+        freq.put(val,f);
+        maxFreq = Math.max(maxFreq, f);
+        
+        stacks.putIfAbsent(f, new Stack<>());
+        stacks.get(f).push(val);
     }
-
+    
     public int pop() {
-        int x = group.get(maxfreq).pop();
-        freq.put(x, freq.get(x) - 1);
-        if (group.get(maxfreq).size() == 0)
-            maxfreq--;
-        return x;
+        int out = stacks.get(maxFreq).pop();
+        freq.put(out, freq.get(out)-1);
+        if(stacks.get(maxFreq).isEmpty()) {
+            maxFreq--;
+        }
+        
+        return out;
     }
 }
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
