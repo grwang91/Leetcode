5ad11cbc2bee9444a7746d6470c461c6e9d2128c
grwang <53140990+grwang91@users.noreply.github.com>
2022-01-30 00:17:10 +0900

    Time: 4 ms (51.97%), Space: 42.3 MB (23.03%) - LeetHub

--- 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.java
+++ 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.java
@@ -1,21 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public void inorder(TreeNode r, List<Integer> nums, Queue<Integer> heap, int k) {
-        if (r == null) 
-            return;
-                    
-        inorder(r.left, nums, heap, k);
-        heap.add(r.val);
-        if (heap.size() > k) 
-            heap.remove();
-        inorder(r.right, nums, heap, k);
-    }
-
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
-        List<Integer> nums = new ArrayList();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((i1, i2) -> diff(target,i1)-diff(target,i2)>0? -1: 1);
         
-        // init heap 'less close element first'
-        Queue<Integer> heap = new PriorityQueue<>((o1, o2) -> Math.abs(o1 - target) > Math.abs(o2 - target) ? -1 : 1);
-        inorder(root, nums, heap, k);
-        return new ArrayList<>(heap);
+        inorder(root, target, k, pq);
+        
+        return new ArrayList<>(pq);
+    }
+    
+    private void inorder(TreeNode root, double target, int k, PriorityQueue<Integer> pq) {
+        if(root == null) {
+            return;
+        }
+        
+        inorder(root.left, target, k, pq);
+        pq.add(root.val);
+        if(pq.size() > k) {
+            pq.poll();
+        } 
+        
+        inorder(root.right, target, k, pq);
+    }
+    
+    private double diff(double target, int val) {
+        return Math.abs(target-val);
     }
 }
