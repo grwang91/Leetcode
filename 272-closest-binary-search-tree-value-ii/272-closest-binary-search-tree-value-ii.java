ea5ae6e5db80fb208aa660f1517a525c3c46a309
grwang <53140990+grwang91@users.noreply.github.com>
2022-01-30 00:06:37 +0900

    Time: 6 ms (28.30%), Space: 45.9 MB (6.15%) - LeetHub

--- 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.java
+++ 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.java
@@ -1,49 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<Integer> closestKValues(TreeNode root, double target, int k) {
-        List<Integer> ans = new ArrayList<>();
-        
-        inorder(root, target, k, ans);
-        
-        return ans;
-    }
-    
-    private void inorder(TreeNode root, double target, int k, List<Integer> ans) {
-        if(root == null) {
+    public void inorder(TreeNode r, List<Integer> nums, Queue<Integer> heap, int k) {
+        if (r == null) 
             return;
-        }
-        
-        inorder(root.left, target, k, ans);
-        
-        if(ans.size() < k) {
-            ans.add(root.val);
-        } else {
-            if(diff(target, ans.get(0)) < diff(target, ans.get(k-1))) {
-                ans.set(k-1, diff(target, ans.get(k-1)) > diff(target, root.val)? root.val: ans.get(k-1));
-            } else {
-                ans.set(0, diff(target, ans.get(0)) > diff(target, root.val)? root.val: ans.get(0));
-            }
-        }
-        Collections.sort(ans);
-        
-        inorder(root.right, target, k, ans);
+                    
+        inorder(r.left, nums, heap, k);
+        heap.add(r.val);
+        if (heap.size() > k) 
+            heap.remove();
+        inorder(r.right, nums, heap, k);
     }
-    
-    private double diff(double target, int val) {
-        return Math.abs(target-val);
+
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> nums = new ArrayList();
+        
+        // init heap 'less close element first'
+        Queue<Integer> heap = new PriorityQueue<>((o1, o2) -> Math.abs(o1 - target) > Math.abs(o2 - target) ? -1 : 1);
+        inorder(root, nums, heap, k);
+        return new ArrayList<>(heap);
     }
 }
