bfa35a5ae72749fccf7fc7e1db08fa696223548d
grwang <53140990+grwang91@users.noreply.github.com>
2022-01-31 18:42:17 +0900

    Time: 24 ms (29.30%), Space: 197.2 MB (5.05%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,38 +1,20 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        boolean[][] dp = new boolean[s.length()][s.length()];
-        
-        for (int i=0; i<s.length(); i++) {
-            dp[i][i] = true;
-        }
-        
-        for (int i=s.length()-1; i>=0; i--) {
-            for (int j=i+1; j<s.length(); j++) {
-                if(s.charAt(i)==s.charAt(j)) {
-                    if(j-i==1 || dp[i+1][j-1]) {
-                        dp[i][j] = true;
-                    }
-                }
-            }
-        }
-        
-        backTrack(s, 0, dp, new ArrayList<>(), ans);
-        
-        return ans;
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        List<List<String>> result = new ArrayList<>();
+        dfs(result, s, 0, new ArrayList<>(), dp);
+        return result;
     }
-    
-    private void backTrack(String s, int start, boolean[][] dp, ArrayList<String> tmp, List<List<String>> ans) {
-        if(start >= s.length()) {
-            ans.add((ArrayList<String>)tmp.clone());
-            return;
-        }
-        
-        for (int i=start; i<s.length(); i++) {
-            if(dp[start][i]) {
-                tmp.add(s.substring(start,i+1));
-                backTrack(s, i+1, dp, tmp, ans);
-                tmp.remove(tmp.size()-1);
+
+    void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
+        if (start >= s.length()) result.add(new ArrayList<>(currentList));
+        for (int end = start; end < s.length(); end++) {
+            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                currentList.add(s.substring(start, end + 1));
+                dfs(result, s, end + 1, currentList, dp);
+                currentList.remove(currentList.size() - 1);
             }
         }
     }
