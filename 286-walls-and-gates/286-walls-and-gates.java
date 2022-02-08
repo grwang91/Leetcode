7bcfa2bd63b2467c3a2336998a43ec899e69205c
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-08 11:42:41 +0900

    Time: 347 ms (6.83%), Space: 258.5 MB (5.03%) - LeetHub

---
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -0,0 +1,47 @@
+class Solution {
+    int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    public void wallsAndGates(int[][] rooms) {
+        
+        for (int i=0; i<rooms.length; i++) {
+            for (int j=0; j<rooms[0].length; j++) {
+                if(rooms[i][j] == 0) {
+                    bfs(rooms, i, j, new boolean[rooms.length][rooms[0].length]);
+                }
+            }
+        }
+    }
+    
+    private void bfs(int[][] rooms, int row, int col, boolean[][] check) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(row,col,0));
+        check[row][col] = true;
+        
+        while(!q.isEmpty()) {
+            Node current = q.poll();
+            // if(current.dist > rooms[current.row][current.col]) {
+            //     return;
+            // }
+            rooms[current.row][current.col]=Math.min(current.dist, rooms[current.row][current.col]);
+            
+            for (int i=0; i<4; i++) {
+                int nRow = current.row+dir[i][0];
+                int nCol = current.col+dir[i][1];
+                
+                if(nRow>=0 && nRow<rooms.length && nCol>=0 && nCol<rooms[0].length && !check[nRow][nCol] && rooms[nRow][nCol] >0) {
+                    q.add(new Node(nRow,nCol, current.dist+1));
+                    check[nRow][nCol]= true;
+                }
+            }
+        }
+    }
+    
+    class Node {
+        int row, col, dist;
+        
+        public Node(int row, int col, int dist) {
+            this.row = row;
+            this.col = col;
+            this.dist = dist;
+        }
+    }
+}
