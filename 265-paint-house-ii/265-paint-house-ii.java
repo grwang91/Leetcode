e077bfa22df1bb3e4eb29bf0e477d14bbc04fefe
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-09 19:16:19 +0900

    Time: 3 ms (70.54%), Space: 46 MB (12.32%) - LeetHub

---
+++ 265-paint-house-ii/265-paint-house-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int minCostII(int[][] costs) {
+        int n = costs.length;
+        int k = costs[0].length;
+        int[][] dp = new int[n][k];
+        
+        int min = Integer.MAX_VALUE;
+        int secondMin = 0;
+        int minColor = 0;
+        
+        for (int i=0; i<k; i++) {
+            dp[0][i] = costs[0][i];
+            if(dp[0][i] < min) {
+                secondMin = min;
+                min = dp[0][i];
+                minColor = i;
+            } else if(dp[0][i] < secondMin){
+                secondMin = dp[0][i];
+            }
+        }
+        
+        for (int i=1; i<n; i++) {
+            int tmpMin = Integer.MAX_VALUE;
+            int tmpSecondMin = 0;
+            int tmpMinColor = 0;
+            for (int j=0; j<k; j++) {
+                dp[i][j] = j==minColor? secondMin+costs[i][j]: min+costs[i][j];
+                if(dp[i][j] < tmpMin) {
+                    tmpSecondMin = tmpMin;
+                    tmpMin = dp[i][j];
+                    tmpMinColor = j;
+                } else if (dp[i][j] < tmpSecondMin) {
+                    tmpSecondMin = dp[i][j];
+                }
+            }
+            min = tmpMin;
+            secondMin = tmpSecondMin;
+            minColor = tmpMinColor;
+        }
+        
+        // for(int i=0; i<n; i++) {
+        //     for (int j=0; j<k; j++) {
+        //         System.out.print(dp[i][j]+" " );
+        //     }
+        //     System.out.println();
+        // }
+        
+        return min;
+    }
+}
