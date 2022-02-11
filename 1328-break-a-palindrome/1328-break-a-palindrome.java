090d6f542eabb44a0fba4ed03ad4306fce6d3413
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-11 13:18:05 +0900

    Time: 6 ms (8.62%), Space: 41.8 MB (16.75%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,20 +1,17 @@
 class Solution {
     public String breakPalindrome(String palindrome) {
-        int length = palindrome.length();
-        if (length == 1) { 
+        if(palindrome.length() == 1) {
             return "";
         }
-        // Strings are immutable in Java, convert it into a char array
-        char[] palindromeArray = palindrome.toCharArray();
         
-        for (int i = 0; i < length / 2; i++) {
-            if (palindromeArray[i] != 'a') {
-                palindromeArray[i] = 'a';
-                return String.valueOf(palindromeArray);
-            }
+        for (int i=0; i<palindrome.length()/2; i++) {
+            char c = palindrome.charAt(i);
+            
+            if(c=='a' || palindrome.length()%2==1 && i == palindrome.length()/2) continue;
+            
+            return palindrome.substring(0,i)+"a"+palindrome.substring(i+1);
         }
         
-        palindromeArray[length - 1] = 'b';
-        return String.valueOf(palindromeArray);
+        return palindrome.substring(0,palindrome.length()-1)+"b";        
     }
 }
