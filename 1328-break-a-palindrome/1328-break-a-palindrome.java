e3bbb20ae1a550d450b932a9e86f22de0196efee
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-11 13:16:12 +0900

    Time: 7 ms (5.30%), Space: 42.1 MB (11.21%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        if(palindrome.length() == 1) {
+            return "";
+        }
+        
+        for (int i=0; i<palindrome.length(); i++) {
+            char c = palindrome.charAt(i);
+            
+            if(c=='a' || palindrome.length()%2==1 && i == palindrome.length()/2) continue;
+            
+            return palindrome.substring(0,i)+"a"+palindrome.substring(i+1);
+        }
+        
+        return palindrome.substring(0,palindrome.length()-1)+"b";        
+    }
+}
