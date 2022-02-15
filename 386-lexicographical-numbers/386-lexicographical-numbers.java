bf5e642acd73d51a4b8cfb8f7f166105872f3f46
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-15 14:19:52 +0900

    Time: 4 ms (75.16%), Space: 46.3 MB (67.31%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.java
+++ 386-lexicographical-numbers/386-lexicographical-numbers.java
@@ -1,41 +1,22 @@
-class trie{
-    trie[] child;
-    boolean isEnd;
-    int val;
-    trie(){
-        child=new trie[10];
-        isEnd=false;
-        val=-1;
-    }
-}
 class Solution {
-    trie root;
-    public void traverse(trie root,List<Integer> res){
-        if(root.isEnd){
-            res.add(root.val);
-        } 
-        for(int i=0;i<10;i++){
-            if(root.child[i]!=null){
-                traverse(root.child[i],res);
-            }
-          
-        }
-    }
     public List<Integer> lexicalOrder(int n) {
-        root=new trie();
-        for(int i=1;i<=n;i++){
-            trie cur=root;
-            for(char c:Integer.toString(i).toCharArray()){
-                int k=c-'0';
-                if(cur.child[k]==null)
-                    cur.child[k]=new trie();
-                cur=cur.child[k];
-            }
-            cur.val=i;
-            cur.isEnd=true;
+        List<Integer> ans = new ArrayList<>();
+        
+        for (int i=1; i<=9; i++) {
+            dfs(i, ans, n);
         }
-        List<Integer> res=new ArrayList<>();
-        traverse(root,res);
-        return res;
+        return ans;
+    }
+    
+    private void dfs(int num, List<Integer> ans, int n) {
+        if(num > n) {
+            return;
+        }
+        
+        ans.add(num);
+        
+        for(int i=0; i<=9; i++) {
+            dfs(10*num+i, ans, n);
+        }
     }
 }
