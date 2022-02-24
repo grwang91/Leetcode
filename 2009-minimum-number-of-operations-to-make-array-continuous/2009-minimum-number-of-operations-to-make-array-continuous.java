0bc6486909c6dfa4b6c23ba694ae348f0b02b984
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-25 00:38:30 +0900

    Time: 186 ms (22.18%), Space: 126.9 MB (12.41%) - LeetHub

---
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+            if(set.contains(nums[i])) {
+                nums[i] = Integer.MAX_VALUE;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        Arrays.sort(nums);
+        int ans = nums.length;
+        
+        for(int i=0; i<nums.length; i++) {
+            int op = i;
+            int max = nums[i]+nums.length-1;
+            
+            int s = i;
+            int e = nums.length;
+            while(s<e) {
+                int m = (s+e)/2;
+                if(nums[m] <= max) {
+                    s = m+1;
+                } else {
+                    e = m;
+                }
+            }
+            op+=nums.length-(e);
+            // System.out.println(e+1);
+            ans = Math.min(ans,op);
+        }
+        return ans;
+    }
+}
