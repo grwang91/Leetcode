d304fd2c3ae9661f6b96a2df9e1b9b6d639bd0a8
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-25 22:09:38 +0900

    Time: 6 ms (77.09%), Space: 69.1 MB (24.60%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,30 +1,22 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-        int[][] prefix = new int[matrix.length+1][matrix[0].length+1];
-        int ans = 0;
-        
-        for(int i=1; i<=matrix.length; i++) {
-            for (int j=1; j<=matrix[0].length; j++) {
-                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + matrix[i-1][j-1];
-            }
+        int ans = -matrix[0][0];
+        for(int i=0; i<matrix.length; i++) {
+            ans+=matrix[i][0];
+        }
+        for(int i=0; i<matrix[0].length; i++) {
+            ans+=matrix[0][i];
         }
         
-        for(int i=1; i<prefix.length; i++) {
-            for(int j=1; j<prefix[0].length; j++) {
-                for(int k=1; k<=matrix.length; k++) {
-                    if(j+k-1 >= prefix[0].length || i+k-1 >= prefix.length) continue;
-                    int val = sum(prefix, i, j, k);
-                    if(val == k*k) {
-                        
-                        ans++;
-                    }
+        for(int i=1; i<matrix.length; i++) {
+            for(int j=1; j<matrix[0].length; j++) {
+                if(matrix[i][j] == 1) {
+                    matrix[i][j] = Math.min(matrix[i-1][j], Math.min(matrix[i-1][j-1], matrix[i][j-1]))+1;
                 }
+                ans+=matrix[i][j];
             }
         }
         return ans;
     }
-    
-    private int sum(int[][] prefix, int r, int c, int n) {
-        return prefix[r+n-1][c+n-1] - prefix[r-1][c+n-1] - prefix[r+n-1][c-1] + prefix[r-1][c-1];
-    }
+
 }
