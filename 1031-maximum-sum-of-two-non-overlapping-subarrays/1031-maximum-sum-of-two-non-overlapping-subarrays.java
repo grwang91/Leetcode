797def9bbb15918d5f2f836a93dc2c82ad80d6a0
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-25 23:53:03 +0900

    Time: 1 ms (91.37%), Space: 43 MB (16.73%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.java
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.java
@@ -1,21 +1,24 @@
 class Solution {
     public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
         int[] prefix = new int[nums.length+1];
-        int[][] dp = new int[nums.length][nums.length];
         int ans = 0;
         
         for(int i=1; i<=nums.length; i++) {
             prefix[i]=prefix[i-1]+nums[i-1];
         }
         
-        // dp[i][j] is maximum sum when firstLen starts at 'i', secondLen starts at 'j'
-        for(int i=0; i<nums.length; i++) {
-            for(int j=0; j<nums.length; j++) {
-                if(i==j || i>j && i<j+secondLen || i<j && j<firstLen+i ) continue;
-                
-                dp[i][j] = prefix[Math.min(prefix.length-1,i+firstLen)]-prefix[i]+prefix[Math.min(prefix.length-1,j+secondLen)]-prefix[j];
-                ans = Math.max(ans,dp[i][j]);
-            }
+        int leftMaxSum = 0;
+        for(int i=firstLen; i<=nums.length-secondLen; i++) {
+            int val = prefix[i]-prefix[i-firstLen];
+            leftMaxSum = Math.max(leftMaxSum, val);
+            ans = Math.max(ans, leftMaxSum+prefix[i+secondLen]-prefix[i]);
+        }
+        
+        int rightMaxSum = 0;
+        for(int i=secondLen; i<=nums.length-firstLen; i++) {
+            int val = prefix[i]-prefix[i-secondLen];
+            rightMaxSum = Math.max(rightMaxSum, val);
+            ans = Math.max(ans, rightMaxSum+prefix[i+firstLen]-prefix[i]);
         }
         
         return ans;
