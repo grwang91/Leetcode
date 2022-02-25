3eda37c4fe56de504c9e2218b85927391f9be11d
grwang <53140990+grwang91@users.noreply.github.com>
2022-02-25 22:22:23 +0900

    Time: 0 ms (100.00%), Space: 43.5 MB (73.88%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
         
-        int pre = nums[0];
-        int cnt1 = 0;
-        for(int i=1; i<nums.length; i++) {
-            if(pre > nums[i]) {
-                cnt1++;
-            }
-            pre = Math.max(pre,nums[i]);
-        }
-        
-        int cnt2 = 0;
-        pre = nums[nums.length-1];
-        for(int i=nums.length-2; i>=0; i--) {
-            if(nums[i] > pre) {
-                cnt2++;
-                pre = Math.min(pre,nums[i]);
-            } else {
-                pre = nums[i];
-            }
+        int numViolations = 0;
+        for (int i = 1; i < nums.length; i++) {
             
+            if (nums[i - 1] > nums[i]) {
+                
+                if (numViolations == 1) {
+                    return false;
+                }
+                
+                numViolations++;
+                
+                if (i < 2 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
         }
         
-        return Math.min(cnt1, cnt2) < 2;
+        return true;
     }
 }
