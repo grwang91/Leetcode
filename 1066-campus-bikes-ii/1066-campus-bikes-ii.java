a98183447e49715e3ff2fbbc6b10591af31d5d88
grwang <53140990+grwang91@users.noreply.github.com>
2022-03-12 00:29:30 +0900

    Time: 4 ms (79.71%), Space: 42 MB (36.55%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.java
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.java
@@ -1,61 +1,34 @@
 class Solution {
-    // Maximum value of mask will be 2^(Number of bikes)
-    // And number of bikes can be 10 at max
-    int memo [] = new int[1024];
-    
-    // Count the number of ones using Brian Kernighan’s Algorithm
-    private int countNumOfOnes(int mask) {
-        int count = 0;
-        while (mask != 0) {
-            mask &= (mask - 1);
-            count++;
-        } 
-        return count;
-    }
-    
-    // Manhattan distance
-    private int findDistance(int[] worker, int[] bike) {
-        return Math.abs(worker[0] - bike[0]) + Math.abs(worker[1] - bike[1]);
-    }
-    
-    private int minimumDistanceSum(int[][] workers, int[][] bikes) {
-        int numOfBikes = bikes.length;
-        int numOfWorkers = workers.length;
-        int smallestDistanceSum = Integer.MAX_VALUE;
-
-        // 0 signifies that no bike has been assigned and
-        // Distance sum for not assigning any bike is equal to 0
-        memo[0] = 0;
+    int[][] memo;
+    public int assignBikes(int[][] workers, int[][] bikes) {
+        memo = new int[workers.length][(1<<bikes.length)];
+        for(int i=0; i<memo.length; i++) {
+            Arrays.fill(memo[i],-1);
+        }
         
-        // Traverse over all the possible values of mask
-        for (int mask = 0; mask < (1 << numOfBikes); mask++) {
-            int nextWorkerIndex = countNumOfOnes(mask);
-            
-            // If mask has more number of 1's than the number of workers
-            // Then we can update our answer accordingly
-            if (nextWorkerIndex >= numOfWorkers) {
-                smallestDistanceSum = Math.min(smallestDistanceSum, memo[mask]);
-                continue;
-            }
-            
-            for (int bikeIndex = 0; bikeIndex < numOfBikes; bikeIndex++) {
-                // Checking if the bike at bikeIndex has already been assigned
-                if ((mask & (1 << bikeIndex)) == 0) {
-                    int newMask = (1 << bikeIndex) | mask;
-                    
-                    // Updating the distance sum for newMask
-                    memo[newMask] = Math.min(memo[newMask], memo[mask] + 
-                                             findDistance(workers[nextWorkerIndex], bikes[bikeIndex]));
-                }
+        return backTrack(workers, bikes, 0, 0);
+    }
+    
+    private int backTrack(int[][] workers, int[][] bikes, int idx, int mask) {
+        if(idx == workers.length) {
+            return 0;
+        }
+        
+        if(memo[idx][mask] != -1) return memo[idx][mask];
+        
+        int dist = Integer.MAX_VALUE;
+        for(int i=0; i<bikes.length; i++) {
+            if((mask&(1<<i)) == 0) {
+                int curr = distance(workers[idx],bikes[i]) + backTrack(workers, bikes, idx+1, mask|(1<<i));
+                dist = Math.min(curr,dist);
             }
         }
         
-        return smallestDistanceSum;
+        memo[idx][mask] = dist;
+        return dist;
     }
     
-    public int assignBikes(int[][] workers, int[][] bikes) {      
-        // Initializing the answers for all masks to be INT_MAX
-        Arrays.fill(memo, Integer.MAX_VALUE);
-        return minimumDistanceSum(workers, bikes);
-	}
+    private int distance(int[] worker, int[] bike) {
+        return Math.abs(worker[0]-bike[0])+Math.abs(worker[1]-bike[1]);
+    }
 }
