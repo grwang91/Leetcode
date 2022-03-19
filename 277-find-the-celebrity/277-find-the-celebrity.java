60d11b637a0a2c728922fa7665674a9487a3d0df
grwang <53140990+grwang91@users.noreply.github.com>
2022-03-19 15:53:49 +0900

    Time: 16 ms (87.47%), Space: 54.6 MB (11.12%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -1,23 +1,28 @@
-/* The knows API is defined in the parent class Relation.
-      boolean knows(int a, int b); */
-
 public class Solution extends Relation {
+    
+    private int numberOfPeople;
+    
     public int findCelebrity(int n) {
-        for(int i=0; i<n; i++) {
-            boolean isCelebrity = true;
-            for(int j=0; j<n; j++) {
-                if(i==j) continue;
-                
-                if(knows(i,j) || !knows(j,i)) {
-                    isCelebrity = false;
-                    break;
-                }
-            }
-            if(isCelebrity) {
-                return i;
+        numberOfPeople = n;
+        int celebrityCandidate = 0;
+        for (int i = 0; i < n; i++) {
+            if (knows(celebrityCandidate, i)) {
+                celebrityCandidate = i;
             }
         }
-        
+        if (isCelebrity(celebrityCandidate)) {
+            return celebrityCandidate;
+        }
         return -1;
+    }
+    
+    private boolean isCelebrity(int i) {
+        for (int j = 0; j < numberOfPeople; j++) {
+            if (i == j) continue; // Don't ask if they know themselves.
+            if (knows(i, j) || !knows(j, i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
