1e74bc92cf04c3f8b4efa8cce9856f737f373ea3
grwang <53140990+grwang91@users.noreply.github.com>
2022-03-28 02:36:17 +0900

    Time: 52 ms (36.77%), Space: 111.5 MB (44.11%) - LeetHub

---
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long minimumRemoval(int[] beans) {
+        long ans = Long.MAX_VALUE;
+        Arrays.sort(beans);
+        long sum = 0;
+        for(int i=0; i<beans.length; i++) {
+            sum+=(long)beans[i];
+        }
+        
+        long curSum = 0;
+        long erase = 0;
+        for(int i=0; i<beans.length; i++) {
+            curSum+=(long)beans[i];
+            ans = Math.min(ans,erase+sum-curSum-(long)beans[i]*(beans.length-1-i));
+            // System.out.println(ans);
+            erase+=beans[i];
+        }
+        return ans;
+    }
+}
