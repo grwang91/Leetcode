6c2bf27c331cbb9c04d062e8586e2d3b819011c6
grwang <53140990+grwang91@users.noreply.github.com>
2022-03-29 21:07:30 +0900

    Time: 198 ms (35.63%), Space: 44.4 MB (94.25%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.java
+++ 1416-restore-the-array/1416-restore-the-array.java
@@ -7,12 +7,8 @@ class Solution {
         for(int i=dp.length-2; i>=0; i--) {
             char c = s.charAt(i);
             if(c == '0') continue;
-            
-            if(c-'0' <= k) {
-                dp[i]=(dp[i]+dp[i+1])%mod;
-            }
-            
-            for(int j=i+1; j<Math.min(i+1+pow,s.length()); j++) {
+
+            for(int j=i; j<Math.min(i+1+pow,s.length()); j++) {
                 if(Integer.parseInt(s.substring(i,j+1)) <= k) {
                     dp[i]=(dp[i]+dp[j+1])%mod;
                 }
