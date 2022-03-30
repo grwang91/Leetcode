6024d4107200a60d0346982f7a74de6bcc25d6c4
grwang <53140990+grwang91@users.noreply.github.com>
2022-03-30 22:02:42 +0900

    Time: 11 ms (21.02%), Space: 45.7 MB (9.97%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (i1,i2)->i1[0]-i2[0]);
+        
+        for(int i=0; i<intervals.length-1; i++) {
+            if(intervals[i][1] <= intervals[i+1][0]) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+}
