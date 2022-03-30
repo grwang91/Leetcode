d43b77d20d3dd06db1eafea5b8e367e0d668442a
grwang <53140990+grwang91@users.noreply.github.com>
2022-03-30 20:12:04 +0900

    Time: 65 ms (18.63%), Space: 88.1 MB (29.23%) - LeetHub

---
+++ 1891-cutting-ribbons/1891-cutting-ribbons.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxLength(int[] ribbons, int k) {
+        int ans = 0;
+        int s = 0;
+        int e = 1000001;
+        
+        while(s<e) {
+            int m = (s+e)/2;
+            if(m==0) break;
+            int cur = 0;
+            for(int ribbon: ribbons) {
+                cur+=ribbon/m;
+            }
+            
+            if(cur >= k) {
+                s=m+1;
+                ans = Math.max(ans, m);
+            } else {
+                e=m;
+            }
+        }
+        
+        return ans;
+    }
+}
