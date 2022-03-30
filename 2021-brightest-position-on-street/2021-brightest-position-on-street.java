e89cb0259da0abd3abee5eda46525bd5ecabf713
grwang <53140990+grwang91@users.noreply.github.com>
2022-03-31 00:52:09 +0900

    Time: 247 ms (65.79%), Space: 112 MB (64.15%) - LeetHub

---
+++ 2021-brightest-position-on-street/2021-brightest-position-on-street.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int brightestPosition(int[][] lights) {
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> start = new PriorityQueue<>();
+        PriorityQueue<Integer> end = new PriorityQueue<>();
+        for(int i=0; i<lights.length; i++) {
+            int s = lights[i][0]-lights[i][1];
+            int e = lights[i][0]+lights[i][1];
+            start.add(s);
+            end.add(e);
+        }
+        
+        int overlap = 0;
+        int ans = 0;
+        int curOverlap = 0;
+        
+        while(!start.isEmpty()) {
+            int cur = start.poll();
+            curOverlap++;
+            
+            while(end.peek() < cur) {
+                end.poll();
+                curOverlap--;
+            }
+            if(curOverlap > overlap) {
+                overlap = curOverlap;
+                ans = cur;
+            }
+        }
+        return ans;
+    }
+}
