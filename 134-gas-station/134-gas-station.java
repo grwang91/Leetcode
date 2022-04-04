852c5a8378ed365e0e133097a8c6e3d017eda7f4
grwang <53140990+grwang91@users.noreply.github.com>
2022-04-04 22:19:12 +0900

    Time: 4 ms (29.11%), Space: 80.4 MB (62.69%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -3,15 +3,16 @@ class Solution {
         int total = 0;
         int cur = 0;
         int start = 0;
+        
         for(int i=0; i<gas.length; i++) {
             if(cur < 0) {
-                cur=0;
+                cur = 0;
                 start = i;
             }
-            cur+=gas[i]-cost[i];
             total+=gas[i]-cost[i];
+            cur+=gas[i]-cost[i];
         }
         
-        return total >=0? start: -1;
+        return total>=0? start: -1;
     }
 }
