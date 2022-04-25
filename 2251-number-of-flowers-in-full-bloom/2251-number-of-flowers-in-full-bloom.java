60546d542558ede888fe6762db881975125f8b97
grwang <53140990+grwang91@users.noreply.github.com>
2022-04-25 21:35:52 +0900

    Time: 143 ms (50.00%), Space: 134.7 MB (50.00%) - LeetHub

---
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int[] fullBloomFlowers(int[][] flowers, int[] persons) {
+        PriorityQueue<Integer> start = new PriorityQueue<>();
+        PriorityQueue<Integer> end = new PriorityQueue<>();
+        PriorityQueue<Integer> p = new PriorityQueue<>();
+        int[] ans = new int[persons.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int[] flower: flowers) {
+            start.add(flower[0]);
+            end.add(flower[1]);
+        }
+        for(int person: persons) {
+            p.add(person);
+        }
+        int cnt = 0;
+        while(!p.isEmpty()) {
+            int cur = p.poll();
+            // System.out.println("======="+cur);
+            if(map.containsKey(cur)) {
+                continue;
+            }
+            
+            while(!start.isEmpty() && start.peek() <= cur) {
+                start.poll();
+                cnt++;
+            }
+            // System.out.println(cnt);
+            int e = -1;
+            int endCount = 0;
+            while(!end.isEmpty() && end.peek() < cur) {
+                int ele = end.poll();
+                if(e == ele) {
+                    endCount++;
+                } else {
+                    e = ele;
+                    endCount = 1;
+                }
+                cnt--;
+            }
+            // System.out.println(cnt);
+            if(e == cur) {
+                map.put(cur, cnt);    
+            } else {
+                map.put(cur, cnt);    
+            }
+            
+        }
+        
+        for(int i=0; i<persons.length; i++) {
+            ans[i] = map.get(persons[i]);
+        }
+        return ans;
+    }
+}
