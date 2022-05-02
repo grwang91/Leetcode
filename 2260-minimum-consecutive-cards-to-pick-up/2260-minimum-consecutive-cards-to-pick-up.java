a44d10b1574da2262101c5d7ed1d33aeac7c03ea
grwang <53140990+grwang91@users.noreply.github.com>
2022-05-02 22:38:35 +0900

    Time: 88 ms (71.43%), Space: 163 MB (14.29%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
@@ -1,14 +1,21 @@
 class Solution {
     public int minimumCardPickup(int[] cards) {
         int[] check = new int[1000001];
-        Arrays.fill(check, -1);
         int ans = cards.length+1;
+        int s = 0;
         for(int i=0; i<cards.length; i++) {
             int n = cards[i];
-            if(check[n] != -1) {
-                ans = Math.min(ans, i-check[n]+1);
+            check[n]++;
+            
+            boolean flag = false;
+            while(check[n] >= 2) {
+                flag = true;
+                check[cards[s++]]--;
             }
-            check[n] = i;
+            if(flag) {
+                // System.out.println(i+" " + s);
+                ans = Math.min(ans, i-s+2);
+            }
         }
         return ans==cards.length+1? -1: ans;
     }
