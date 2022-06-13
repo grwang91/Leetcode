8864c3cc515efa8e828c57c64ccdb34f289ca56f
grwang <53140990+grwang91@users.noreply.github.com>
2022-06-13 20:34:39 +0900

    Time: 30 ms (37.74%), Space: 45.5 MB (43.36%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,15 +1,23 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         long ans = 0;
-        
-        for(int i=0; i<nums.length; i++) {
-            int min = nums[i];
-            int max = nums[i];
-            for(int j=i; j<nums.length; j++) {
-                min = Math.min(min,nums[j]);
-                max = Math.max(max, nums[j]);
-                ans+=max-min;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for(int i=0; i<=nums.length; i++) {
+            while(stack.peek() != -1 && (i==nums.length || nums[stack.peek()] > nums[i])) {
+                int pos = stack.pop();
+                ans-=nums[pos]*(long)(i-pos)*(pos-stack.peek());
             }
+            stack.push(i);
+        }
+        stack.clear();
+        stack.push(-1);
+        for(int i=0; i<=nums.length; i++) {
+            while(stack.peek() != -1 && (i==nums.length || nums[stack.peek()] < nums[i])) {
+                int pos = stack.pop();
+                ans+=nums[pos]*(long)(i-pos)*(pos-stack.peek());
+            }
+            stack.push(i);
         }
         
         return ans;
