90c20b60317c5a197d654be0324542d048efdc69
grwang <53140990+grwang91@users.noreply.github.com>
2022-06-15 22:51:10 +0900

    Time: 112 ms (12.04%), Space: 67 MB (33.73%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,42 +1,24 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        int ans = 1;
-        int[] len = new int[words.length];
-        Arrays.fill(len, 1);
+        int ans = 0;
+        Map<String, Integer> map = new HashMap<>();
         Arrays.sort(words, (w1, w2)-> w1.length()-w2.length());
         
         for(int i=0; i<words.length; i++) {
-            for(int j=i+1; j<words.length; j++) {
-                if(predecessor(words[i], words[j])) {
-                    len[j] = Math.max(len[j], len[i]+1);
-                    ans = Math.max(ans, len[j]);
-                }
+            String word = words[i];
+            int curLen = 0;
+            for(int j=0; j<word.length(); j++) {
+                StringBuilder sb = new StringBuilder(word);
+                sb.deleteCharAt(j);
+                String deleted = sb.toString();
+                int len = map.getOrDefault(deleted, 0)+1;
+                curLen = Math.max(len, curLen);
             }
+            
+            map.put(word, curLen);
+            ans = Math.max(ans, curLen);
         }
         
         return ans;
-    }
-    
-    private boolean predecessor(String a, String b) {
-        if(b.length()-a.length() != 1) return false;
-        
-        char added = ' ';
-        int[] cnt = new int[26];
-        
-        for(char c: a.toCharArray()) {
-            cnt[c-'a']++;
-        }
-        for(char c: b.toCharArray()) {
-            cnt[c-'a']--;
-            if(cnt[c-'a'] < 0) {
-                added = c;
-            }
-        }
-        
-        for(int i=0; i<=a.length(); i++) {
-            if((a.substring(0,i)+added+a.substring(i)).equals(b)) return true;
-        }
-        
-        return false;
     }
 }
