6efff9d309948a2c1918871109db2d54939ec669
grwang <53140990+grwang91@users.noreply.github.com>
2022-06-25 16:00:24 +0900

    Time: 19 ms (56.00%), Space: 117.9 MB (78.43%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -1,27 +1,18 @@
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        Map<Integer, Integer> path = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        List<Integer> arr = new ArrayList<>();
+        int[] path = new int[n];
         long sum = 0;
         
         for(int[] road: roads) {
-            path.put(road[0], path.getOrDefault(road[0],0)+1);
-            path.put(road[1], path.getOrDefault(road[1],0)+1);
+            path[road[0]]++;
+            path[road[1]]++;
         }
         
-        for(int key: path.keySet()) {
-            // pq.add(path.get(key));
-            arr.add(path.get(key));
-        }
-        Collections.sort(arr);
+        Arrays.sort(path);
         
-        for(int i=arr.size()-1; i>=0; i--) {
-            sum+=(long)(arr.get(i))*(n--);
+        for(int i=0; i<path.length; i++) {
+            sum+=(long)(path[i])*(i+1);
         }
-        // while(!pq.isEmpty()) {
-        //     sum+=(long)pq.poll()*(n--);
-        // }
         
         return sum;
     }
