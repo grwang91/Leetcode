bb44f227f2251eea519a13ac386d1b440c22b272
grwang <53140990+grwang91@users.noreply.github.com>
2022-07-05 22:04:36 +0900

    Time: 6 ms (62.75%), Space: 54.4 MB (46.75%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,26 +1,32 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();
+        // Map<Integer, Set<Integer>> map = new HashMap<>();
+        boolean[][] map = new boolean[n][n];
+        int[] cnts = new int[n];
         int ans = 0;
         
-        for(int i=0; i<n; i++) {
-            map.put(i, new HashSet<>());
-        }
+        // for(int i=0; i<n; i++) {
+        //     map.put(i, new HashSet<>());
+        // }
         
         for(int[] road: roads) {
             int s = road[0];
             int e = road[1];
             
-            map.get(s).add(e);
-            map.get(e).add(s);
+            map[s][e] = true;
+            map[e][s] = true;
+            cnts[s]++;
+            cnts[e]++;
+            // map.get(s).add(e);
+            // map.get(e).add(s);
         }
         
         for(int i=0; i<n; i++) {
             for(int j=i+1; j<n; j++) {
                 int cnt = 0;
-                cnt+=map.get(i).size()+map.get(j).size();
+                cnt+=cnts[i]+cnts[j];
                 
-                if(map.get(i).contains(j)) {
+                if(map[i][j]) {
                     cnt--;
                 }
                 ans = Math.max(ans, cnt);
