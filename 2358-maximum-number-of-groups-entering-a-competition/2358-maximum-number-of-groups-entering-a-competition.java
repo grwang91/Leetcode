139a1a58d16f16e401a83c774d6612eca29b9407
grwang <53140990+grwang91@users.noreply.github.com>
2022-08-04 23:01:29 +0900

    Time: 0 ms (100.00%), Space: 50.4 MB (98.90%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
@@ -1,36 +1,8 @@
 class Solution {
     public int maximumGroups(int[] grades) {
-        int ans = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int g: grades) {
-            pq.add(g);
-        }
-        
-        int pre = 0;
-        int size = 0;
-        while(pq.size() > size) {
-            int cur = 0;
-            int curSize = 0;
-            for(int i=0; i<size+1; i++) {
-                curSize++;
-                cur+=pq.poll();
-            }
-            
-            while(!pq.isEmpty() && cur <= pre) {
-                curSize++;
-                cur+=pq.poll();
-            }
-            
-            if(cur <= pre) {
-                break;
-            }
-            pre = cur;
-            size = curSize;
-            
-            ans++;
-        }
-        
-        return ans;
+        int k = 0, total = 0, n = grades.length;
+        while (total + k + 1 <= n)
+            total += ++k;
+        return k;
     }
 }
