5217450e3f8e5f97dd3d2c46d18a2e81021927df
grwang <53140990+grwang91@users.noreply.github.com>
2022-08-20 21:43:13 +0900

    Time: 36 ms (37.80%), Space: 45.6 MB (28.20%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.java
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.java
@@ -1,41 +1,43 @@
 class Leaderboard {
-    TreeMap<Integer, Integer> scoreMap;
-    Map<Integer, Integer> map;
+
+    private HashMap<Integer, Integer> scores;
+    
     public Leaderboard() {
-        scoreMap = new TreeMap<>(Collections.reverseOrder());
-        map = new HashMap<>();
+        this.scores = new HashMap<Integer, Integer>();
     }
     
     public void addScore(int playerId, int score) {
-        int pre = map.getOrDefault(playerId, 0);
-        map.put(playerId, pre+score);
-        scoreMap.put(score+pre, scoreMap.getOrDefault(pre+score,0)+1);
-        scoreMap.put(pre, scoreMap.getOrDefault(pre,1)-1);
+        
+        if (!this.scores.containsKey(playerId)) {
+            this.scores.put(playerId, 0);
+        }
+        
+        this.scores.put(playerId, this.scores.get(playerId) + score);
     }
     
     public int top(int K) {
-        int sum = 0;
-        for(Map.Entry<Integer, Integer> entry: scoreMap.entrySet()) {
-            for(int i=0; i<entry.getValue(); i++) {
-                if(K == 0) break;
-                sum+=entry.getKey();
-                K--;
+        
+        // A min-heap in java containing entries of a hash map. Note that we have to provide
+        // a comparator of our own to make sure we get the ordering right of these objects.
+        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        
+        for (Map.Entry<Integer, Integer> entry : this.scores.entrySet()) {
+            heap.offer(entry);
+            if (heap.size() > K) {
+                heap.poll();
             }
         }
-        return sum;
+        
+        int total = 0;
+        Iterator value = heap.iterator();
+        while (value.hasNext()) { 
+            total += ((Map.Entry<Integer, Integer>)value.next()).getValue();   
+        }
+        
+        return total;
     }
     
     public void reset(int playerId) {
-        int score = map.get(playerId);
-        map.remove(playerId);
-        scoreMap.put(score, scoreMap.get(score)-1);
+        this.scores.put(playerId, 0);
     }
 }
-
-/**
- * Your Leaderboard object will be instantiated and called as such:
- * Leaderboard obj = new Leaderboard();
- * obj.addScore(playerId,score);
- * int param_2 = obj.top(K);
- * obj.reset(playerId);
- */
